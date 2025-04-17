e7d29349c6253a854bda0dd75252661d452b7b06
Sebastien Caps <493313+virer@users.noreply.github.com>
2025-04-17 23:18:48 +0200

    Fix manifest name

--- deploy/install-driver.sh
+++ deploy/install-driver.sh
@@ -3,5 +3,5 @@
 echo "Installing virium-iscsi.csi.k8s.io CSI driver..."
 kubectl apply -f csi-iscsi-driverinfo.yaml
 kubectl apply -f rbac.yaml
-kubectl apply -f csi-iscsi-node.yaml
+kubectl apply -f csi-iscsi-daemonset.yaml
 echo 'virium-iscsi.csi.k8s.io CSI driver installed successfully.'
