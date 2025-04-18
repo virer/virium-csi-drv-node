845b4b4317862cc09f9889f6044ac0be6d63d49a
Sebastien Caps <493313+virer@users.noreply.github.com>
2025-04-18 09:45:44 +0200

    Fix version control

--- scripts/newversion.sh
+++ scripts/newversion.sh
@@ -3,7 +3,7 @@
 # Increment version
 TAG=$( cat VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' )
 echo "SWitch to version ${TAG}"
-
+echo ${TAG} > VERSION
 git tag ${TAG}
 sed -i "s/var version =.*/var version = \"${TAG}\"/g" cmd/virium-iscsiplugin/driver.go
 sed -i "s#docker.io/scaps/virium-csi-driver-iscsi.*#docker.io/scaps/virium-csi-driver-iscsi:${TAG}#g" deploy/csi-iscsi-daemonset.yaml
