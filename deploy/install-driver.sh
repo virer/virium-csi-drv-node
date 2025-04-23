a172b2408ec2ab199fe3c9c069709ff5d6f2eec3
Sebastien Caps <493313+virer@users.noreply.github.com>
2025-04-23 13:35:51 +0200

    Removed deploy directory to avoid

--- deploy/install-driver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing virium.csi.virer.net CSI driver..."
-kubectl apply -f csi-virium-driverinfo.yaml
-kubectl apply -f csi-virium-rbac.yaml
-kubectl apply -f csi-virium-daemonset.yaml
-echo 'virium.csi.virer.net CSI driver installed successfully.'
--- deploy/uninstall-driver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Uninstalling virium.csi.virer.net CSI driver ..."
-kubectl delete -f csi-iscsi-driverinfo.yaml
-kubectl delete -f csi-iscsi-node.yaml
-echo 'virium.csi.virer.net CSI driver uninstalled successfully.'
